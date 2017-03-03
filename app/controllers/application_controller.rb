856b516aba62b00faf73204890c4061e389cb79d
Alucard <alucardlogistics@email.com>
2017-03-03 02:25:09 +0000

    added devise and bootstrapt gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #before any action in the app we need an authentifcated user
+  before_action :authenticate_user!
 end
